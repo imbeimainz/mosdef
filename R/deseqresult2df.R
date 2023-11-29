b65644d364aee57e68e697059b67862c0d1dfcd2
Leon Dammer <l.dammer@gmx.de>
2023-11-29 10:41:48 +0100

    removed deseqresult2DE genes and imported deseqresult2df

--- R/deseqresult2df.R
+++ R/deseqresult2df.R
@@ -37,3 +37,40 @@ deseqresult2DEgenes <- function(deseqresult,
   res <- tmp[!(is.na(tmp$padj)) & tmp$padj <= FDR, ]
   res
 }
+
+
+
+#' Generate a table from the `DESeq2` results
+#'
+#' Generate a tidy table with the results of `DESeq2`
+#'
+#' @param res_de A `DESeqResults` object.
+#' @param FDR Numeric value, specifying the significance level for thresholding
+#' adjusted p-values. Defaults to NULL, which would return the full set of results
+#' without performing any subsetting based on FDR.
+#'
+#' @return A tidy `data.frame` with the results from differential expression,
+#' sorted by adjusted p-value. If FDR is specified, the table contains only genes
+#' with adjusted p-value smaller than the value.
+#'
+#' @export
+#'
+#' @examples
+#' data(res_de_macrophage, package = "GeneTonic")
+#' head(res_macrophage_IFNg_vs_naive)
+#' res_df <- deseqresult2df(res_macrophage_IFNg_vs_naive)
+#' head(res_df)
+deseqresult2df <- function(res_de, FDR = NULL) {
+  if (!is(res_de, "DESeqResults")) {
+    stop("Not a DESeqResults object.")
+  }
+  res <- as.data.frame(res_de)
+  res <- cbind(rownames(res), res)
+  names(res)[1] <- "id"
+  res$id <- as.character(res$id)
+  res <- res[order(res$padj), ]
+  if (!is.null(FDR)) {
+    res <- res[!(is.na(res$padj)) & res$padj <= FDR, ]
+  }
+  res
+}
