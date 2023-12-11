a487cc08bc75d09620018eebd14d50764a3a10a2
Leon Dammer <l.dammer@gmx.de>
2023-12-11 10:58:57 +0100

    create a de_volcano plot that returns a ggplot object which users can then further adapt

---
+++ R/de_volcano.R
@@ -0,0 +1,87 @@
+#' Generates a volcano plot using ggplot2
+#' This function generates a base volcanoplot for differentially expressed genes that can then be expanded
+#' upon using further ggplot functions.
+#'
+#' @param res_de A DESeqResults object created using \code{DESeq2} 
+#' @param L2FC_cutoff A numeric value that sets the cutoff for the xintercept argument of ggplot
+#' @param labeled_genes A numeric value describing the amount of genes to be labeled. This uses the Top(x) highest differnetially expressed genes 
+#' @param mapping Which \code{org.XX.eg.db} to use for annotation - select according to the species
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(tidyverse) # includes ggplot2, for data visualisation. dplyr, for data manipulation.
+#' library(RColorBrewer) # for a colourful plot
+#' library(ggrepel) # for nice annotations
+#' library(airway)
+#' library(DESeq2)
+#' data(airway)
+#' airway
+#' dds_airway <- DESeqDataSet(airway, design= ~ cell + dex)
+#' # Example, performing extraction of enriched functional categories in
+#' # detected significantly expressed genes
+#' ## Not run: 
+#' dds_airway <- DESeq(dds_airway)
+#' res_airway <- results(dds_airway)
+#' de_volcano(res_airway, 
+#'            L2FC_cutoff = 2,
+#'            labeled_genes = 20,
+#'            mapping = "org.Hs.eg.db")
+
+
+de_volcano <- function(res_de,
+                       L2FC_cutoff = 1, 
+                       labeled_genes = 30,
+                       mapping = "org.Mm.eg.db" ){
+
+  #library(package = mapping)
+  library("AnnotationDbi")
+  library("org.Hs.eg.db")
+  res_de$symbol <- mapIds(org.Hs.eg.db,
+                      keys = row.names(res_airway),
+                      column = "SYMBOL",
+                      keytype = "ENSEMBL",
+                      multiVals = "first")
+  
+  df <-  deseqresult2df(res_de)
+  
+  if(max(df$log2FoldChange, na.rm = TRUE) >= abs(min(df$log2FoldChange, na.rm = TRUE))){
+    #checking which absolute value is bigger to set xlim
+    
+    x_limit = ceiling(max(df$log2FoldChange, na.rm = TRUE))
+    
+  }else if(max(df$log2FoldChange, na.rm = TRUE) < abs(min(df$log2FoldChange, na.rm = TRUE))){
+    
+    x_limit = ceiling(abs(min(df$log2FoldChange, na.rm = TRUE)))
+    
+  }
+  
+  df$diffexpressed <- "NO"
+  # if log2Foldchange > 0.6 and pvalue < 0.05, set as "UP"
+  df$diffexpressed[df$log2FoldChange > L2FC_cutoff & df$padj < 0.05] <- "UP"
+  # if log2Foldchange < -0.6 and pvalue < 0.05, set as "DOWN"
+  df$diffexpressed[df$log2FoldChange < -L2FC_cutoff & df$padj < 0.05] <- "DOWN"
+  
+  #calculate top 30 degenes based on padj
+  df$delabel <- ifelse(df$symbol %in% head(df[order(df$padj), "symbol"], labeled_genes), df$symbol, NA)  
+  
+  
+  
+  
+  ggplot(data = df, aes(x = log2FoldChange, y = -log10(padj), colour = diffexpressed, label = delabel)) +
+    geom_vline(xintercept = c(-L2FC_cutoff, L2FC_cutoff), col = "gray", linetype = 'dashed') +
+    geom_hline(yintercept = -log10(0.05), col = "gray", linetype = 'dashed') + 
+    geom_point()+
+    theme_classic()+
+    scale_color_manual(values = c("skyblue", "grey", "tomato"), # to set the colours of our variable
+                       labels = c("Downregulated", "Not significant", "Upregulated"))+
+    coord_cartesian(ylim = c(0, 250), xlim = c(-x_limit, x_limit))+
+    scale_x_continuous(breaks = seq(-x_limit, x_limit, 2))+
+    geom_text_repel(max.overlaps = Inf) # To show all labels 
+}
+
+
+
+
+
