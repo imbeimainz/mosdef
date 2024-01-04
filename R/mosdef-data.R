4eda1aec561af652e572382fb206dcc7fe4b0ad9
Federico Marini <marinif@uni-mainz.de>
2024-01-04 11:29:04 +0100

    placeholder for data object to be provided in the package

---
+++ R/mosdef-data.R
@@ -0,0 +1,18 @@
+#' A sample `DESeqResults` object
+#'
+#' A sample `DESeqResults` object, generated in the `DESeq2` framework
+#'
+#' @details This `DESeqResults` object on the data from the `macrophage` package
+#' has been created comparing IFNg treated samples vs naive samples, accounting
+#' for the different cell lines included.
+#'
+#' Details on how this object has been created are included in the `create_gt_data.R`
+#' script, included in the `scripts` folder of the `GeneTonic` package.
+#'
+#' @references Alasoo, et al. "Shared genetic effects on chromatin and gene
+#' expression indicate a role for enhancer priming in immune response",
+#' Nature Genetics, January 2018 doi: 10.1038/s41588-018-0046-7.
+#'
+#' @name res_macrophage_IFNg_vs_naive
+#' @docType data
+NULL
