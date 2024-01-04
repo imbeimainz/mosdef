1932f534e989260ecae42c7e72c2023906f904cd
Federico Marini <marinif@uni-mainz.de>
2024-01-04 11:29:14 +0100

    yaddayadda

--- R/gene_symbol_buttons.R
+++ R/gene_symbol_buttons.R
@@ -1,24 +1,24 @@
 #' Create a button for genesymbols in an RMD
-#' 
+#'
 #' A function to turn Gene Symbols into buttons in an RMD linking to various Portals for further info
-#' about these genes. 
+#' about these genes.
 #' Current supported portals are: Genecards, NCBI, GTEX, Uniprot, dbPTM, Human Protein Atlas
 #'
 #' @param df A dataframe with at least on coloumn with gene Symbols named: SYMBOL
-#' @param new_cols At least one of: "GC", "NCBI", "GTEX", "UNIPROT", "dbPTM", "HPA" 
+#' @param new_cols At least one of: "GC", "NCBI", "GTEX", "UNIPROT", "dbPTM", "HPA"
 #' @param col_to_use name of the coloumns were the gene symbols are stored. Default is SYMBOL
 #' @param output_format a parameter deciding which output format to return, either a DT:datatable (recommended)
 #' or a simple dataframe (DF).In the latter case it is important that if the data is visualized with the
 #'  \code{datatable} function the parameter escape must be set to FALSE
 #'
-#' @return
+#' @return TODO
 #' @export
 #'
 #' @examples
 #' library(dplyr)
 #' library(DESeq2)
 #' data("gse", package = "macrophage")
-#' 
+#'
 #' dds_macrophage <- DESeqDataSet(gse, design = ~line + condition)
 # changing the ids to Ensembl instead of the Gencode used in the object
 #' rownames(dds_macrophage) <- substr(rownames(dds_macrophage), 1, 15)
@@ -27,7 +27,7 @@
 #' dds_macrophage <- dds_macrophage[keep, ]
 #' dds_macrophage
 #' dds_macrophage <- DESeq(dds_macrophage)
-#' 
+#'
 #' res_macrophage_IFNg_vs_naive <- results(dds_macrophage,
 #'                                         contrast = c("condition", "IFNg", "naive"),
 #'                                         lfcThreshold = 1, alpha = 0.05)
@@ -38,7 +38,7 @@
 
 
 gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = "SYMBOL", output_format = "DT"){
-  
+
   .actionbutton_biocstyle <- "color: #ffffff; background-color: #0092AC"
   val <- df[[col_to_use]]
   match.arg(new_cols,choices = c("GC", "NCBI", "GTEX", "UNIPROT", "dbPTM", "HPA"), several.ok = TRUE)
@@ -46,8 +46,8 @@ gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = 
   #GeneCards
   if( "GC" %in% new_cols){
     for (i in 1:length(df$SYMBOL)) {
-      
-      
+
+
       df$SYMBOL_GC[i] <- sprintf(
         '<a href = "https://www.genecards.org/cgi-bin/carddisp.pl?gene=%s" target = "_blank" class = "btn     btn-primary" style = "%s">%s</a>',
         val[i],
@@ -56,22 +56,22 @@ gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = 
       )
     }
   }
-  
+
   #NCBI
   if( "NCBI" %in% new_cols){
-    
-    
+
+
     for (i in 1:length(df$SYMBOL)) {
-      
+
       df$SYMBOL_NCBI[i] <- paste0('<a href="http://www.ncbi.nlm.nih.gov/gene/?term=',val[i],'[sym]" target="_blank" class="btn btn-primary">',
                                   paste0(val[i], "@NCBI"),'</a>')
     }
   }
-  
-  #GTEx 
+
+  #GTEx
   if( "GTEX" %in% new_cols){
-    
-    
+
+
     for (i in 1:length(df$SYMBOL)) {
       df$SYMBOL_GTEX[i] <- sprintf(
         '<a href = "https://www.gtexportal.org/home/gene/%s" target = "_blank" class = "btn btn-primary" style = "%s"><i class="fa fa-dna"></i>%s</a>',
@@ -81,11 +81,11 @@ gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = 
       )
     }
   }
-  
+
   #Uniprot
   if( "UNIPROT" %in% new_cols){
-    
-    
+
+
     for (i in 1:length(df$SYMBOL)) {
       df$SYMBOL_UniProt[i] <- sprintf('<a href = "https://www.uniprot.org/uniprot/?query=%s" target = "_blank" class = "btn btn-primary" style = "%s"><i class="fa fa-spinner"></i>%s</a>',
                                       val[i],
@@ -94,12 +94,12 @@ gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = 
       )
     }
   }
-  
-  #dbPTM 
+
+  #dbPTM
   if( "dbPTM" %in% new_cols){
     base_link_old <- "http://dbptm.mbc.nctu.edu.tw/"
     base_link_new <- "https://awi.cuhk.edu.cn/dbPTM/"
-    
+
     for (i in 1:length(df$SYMBOL)) {
       df$SYMBOL_dbPTM[i] <-   sprintf('<a href = "%s/info.php?id=%s_HUMAN" target = "_blank" class = "btn btn-primary" style = "%s"><i class="fa fa-edit"></i>%s</a>',
                                       base_link_new,     # main link to website
@@ -108,11 +108,11 @@ gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = 
                                       paste0(val[i], "@dbPTM")) # content of the button label
     }
   }
-  
+
   #Human protein atlas Portal
   if( "HPA" %in% new_cols){
-    
-    
+
+
     for (i in 1:length(df$SYMBOL)) {
       df$SYMBOL_HPA[i] <- sprintf('<a href = "https://www.proteinatlas.org/search/%s" target = "_blank" class = "btn btn-primary" style = "%s"><i class="fa fa-cubes"></i>%s</a>',
                                   val[i],
@@ -124,12 +124,12 @@ gene_symbol_buttons <- function(df, new_cols = c("GC", "UNIPROT"), col_to_use = 
     select(-SYMBOL)
   if (output_format == "DT"){
   return(DT::datatable(df, escape = FALSE))
-  
+
   }else if (output_format == "DF"){
- 
+
   return(df)
   }
-  
+
 }
 
 
