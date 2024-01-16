f088e67f2b9dd256f1d35556721502524389a3d5
Leon Colin Dammer <l.dammer@gmx.de>
2024-01-16 13:51:18 +0100

    Added a test to see if top_de works

--- tests/testthat/test-cluProtable.R
+++ tests/testthat/test-cluProtable.R
@@ -38,5 +38,25 @@ test_that("enrich_result is created  when using vectors not res_De/dds", {
   expect_s4_class(CluProde_airway_vec, "enrichResult")
   
 })
+
+test_that("top_de works propperly", {
+    CluProde_airway_topde <- cluproTable(
+      res_de = res_airway,
+      dds = dds_airway,
+      mapping = "org.Hs.eg.db",
+      top_de = 400
+    )
+    expect_s4_class(CluProde_airway_topde, "enrichResult")
+  
+    CluProde_airway_topde_vectors <- cluproTable(
+      de_genes =  myde,
+      bg_genes  = myassayed,
+      mapping = "org.Hs.eg.db",
+      top_de = 400,
+      keyType = "ENSEMBL"
+    )
+    expect_s4_class(CluProde_airway_topde_vectors, "enrichResult")
+  
+})
   
   
