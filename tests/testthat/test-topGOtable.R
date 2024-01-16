53f2c88c0dbf56840b7e3c09cab3da7673707dbc
Leon Colin Dammer <l.dammer@gmx.de>
2024-01-16 16:12:37 +0100

    Unit-testing for topGO table

---
+++ tests/testthat/test-topGOtable.R
@@ -0,0 +1,72 @@
+test_that("enrich_result is created", {
+  topGOde_airway <- topGOtable(
+    res_de = res_airway,
+    dds = dds_airway,
+    mapping = "org.Hs.eg.db"
+  )
+  expect_s3_class(topGOde_airway, "data.frame")
+  
+  
+})
+
+test_that("enrich_result is created only for up or down -regulated genes", {
+  topGOde_airway_up <- topGOtable(
+    res_de = res_airway,
+    dds = dds_airway,
+    mapping = "org.Hs.eg.db",
+    de_type = "up"
+  )
+  expect_s3_class(topGOde_airway_up, "data.frame")
+  
+  topGOde_airway_down <- topGOtable(
+    res_de = res_airway,
+    dds = dds_airway,
+    mapping = "org.Hs.eg.db",
+    de_type = "down"
+  )
+  expect_s3_class(topGOde_airway_down, "data.frame")
+  
+})
+
+test_that("enrich_result is created  when using vectors not res_De/dds", {
+  topGOde_airway_vec <- topGOtable(
+    de_genes  =  myde,
+    bg_genes  = myassayed,
+    mapping = "org.Hs.eg.db"
+  )
+  expect_s3_class(topGOde_airway_vec, "data.frame")
+  
+})
+
+test_that("top_de works propperly", {
+  topGOde_airway_topde <- topGOtable(
+    res_de = res_airway,
+    dds = dds_airway,
+    mapping = "org.Hs.eg.db",
+    top_de = 400
+  )
+  expect_s3_class(topGOde_airway_topde, "data.frame")
+  
+  topGOde_airway_topde_vectors <- topGOtable(
+    de_genes =  myde,
+    bg_genes  = myassayed,
+    mapping = "org.Hs.eg.db",
+    top_de = 400
+  )
+  expect_s3_class(topGOde_airway_topde_vectors, "data.frame")
+  
+})
+
+test_that("Error is thrown if wrong topGO method is used", {
+  
+  expect_error(topGOde_airway_methods <- topGOtable(
+    res_de = res_airway,
+    dds = dds_airway,
+    mapping = "org.Hs.eg.db",
+    top_go_method2 = "test"
+    )
+  )
+  
+  
+})
+
