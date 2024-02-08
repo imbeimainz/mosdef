a6eda87c9e30677d9dbe3aab9374ee16c0e3f163
Leon Colin Dammer <l.dammer@gmx.de>
2024-02-08 14:26:44 +0100

    Added documentation for the data objects (although it doesn't work yet and still gives a warning)

--- R/mosdef-data.R
+++ R/mosdef-data.R
@@ -2,18 +2,45 @@
 #'
 #' A sample `DESeqResults` object, generated in the `DESeq2` framework
 #'
-#' @details This `DESeqResults` object on the data from the `macrophage` package
-#' has been created comparing IFNg treated samples vs naive samples, accounting
-#' for the different cell lines included.
+#' @details This `DESeqResults` object is on the data from the `airway` package
 #'
-#' Details on how this object has been created are included in the `create_gt_data.R`
-#' script, included in the `scripts` folder of the `GeneTonic` package.
 #'
-#' @references Alasoo, et al. "Shared genetic effects on chromatin and gene
-#' expression indicate a role for enhancer priming in immune response",
-#' Nature Genetics, January 2018 doi: 10.1038/s41588-018-0046-7.
+#' Details on how this object has been created are included in the `create_mosdef_data`
+#' script, included in the `scripts` folder of the `mosdef` package.
+#'
+#' @references TODO
 #'
 #' @name res_airway
+#' @docType data 
+#' 
+#' A sample `DESeqDataset` objecz
+#' 
+#' A sample `DESeqDataset` object, generated in the `DESeq2` framework
+#'
+#' @details This `DESeqDataset` object is on the data from the `airway` package
+#'
+#'
+#' Details on how this object has been created are included in the `create_mosdef_data`
+#' script, included in the `scripts` folder of the `mosdef` package.
+#' 
+#' @references TODO
+#' 
 #' @name dds_airway
 #' @docType data
+#' 
+#' A sample enrichment object
+#' 
+#' A sample enrichment object, generated in the `mosdef` and `topGO` framework
+#' 
+#' @details This enrichment object is on the data from the `airway` package
+#'
+#'
+#' Details on how this object has been created are included in the `create_mosdef_data`
+#' script, included in the `scripts` folder of the `mosdef` package.
+#' 
+#' @references TODO
+#' 
+#' @name topgoDE_airway
+#' @docType data
+#' 
 NULL
