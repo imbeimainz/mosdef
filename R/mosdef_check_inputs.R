f1716098203d8cd5807255352659164cbba6e66b
Leon Dammer <l.dammer@gmx.de>
2024-03-11 13:40:19 +0100

    Added runnable examples

--- R/mosdef_check_inputs.R
+++ R/mosdef_check_inputs.R
@@ -7,8 +7,8 @@
 #' @export
 #'
 #' @examples
-#' # TODO: load data
-#' # mosdef_res_check(res_de)
+#' data(res_de_macrophage, package = "mosdef")
+#' mosdef_res_check(res_macrophage_IFNg_vs_naive)
 mosdef_res_check <- function(res_de,
                              verbose = FALSE) {
   if ("symbol" %in% colnames(res_de)) {
@@ -64,8 +64,15 @@ mosdef_res_check <- function(res_de,
 #' @export
 #'
 #' @examples
-#' # TODO: load data
-#' # mosdef_dds_check(dds)
+#' library(macrophage)
+#' library(DESeq2)
+#' data(gse)
+#' dds_macrophage <- DESeqDataSet(gse, design = ~ line + condition)
+#' rownames(dds_macrophage) <- substr(rownames(dds_macrophage), 1, 15)
+#' keep <- rowSums(counts(dds_macrophage) >= 10) >= 6
+#' dds_macrophage <- dds_macrophage[keep, ]
+#' dds_macrophage <- DESeq(dds_macrophage)
+#' mosdef_dds_check(dds_macrophage)
 mosdef_dds_check <- function(dds,
                              verbose = FALSE) {
   if (all(grep(pattern = "^ENS", rownames(dds)))) {
@@ -74,12 +81,12 @@ mosdef_dds_check <- function(dds,
     message("Rownames are not ENSEMBL IDs. Please change them to ENSEMBL IDs by using
             AnnotationDbis 'mapIDs'")
   }
-  if ("counts" %in% colnames(dds)) {
+  if ("counts" %in% names(assays(dds_macrophage))) {
     # TODO: we probably need to work on assays (and rowData, and a bit more)
-    if (verbose) message("Found a 'counts' column!")
+    if (verbose) message("Found a 'counts' assay!")
   } else {
-    message("Could not find a 'counts' column. Please ensure you have these values
-          and rename the column: 'counts'")
+    message("Could not find a 'counts' assay Please ensure you have these values
+          and rename the column/assay: 'counts'")
   }
 
 
